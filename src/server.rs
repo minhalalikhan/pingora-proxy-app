6e5e63431bf45cb3a91f549f07239ea90713fef9
unknown <minhalalikhan.786@gmail.com>
2025-02-15 01:14:24 +0530

    init

---
+++ src/server.rs
@@ -0,0 +1,25 @@
+use axum::http::HeaderMap;
+use axum::routing::get;
+use axum::Router;
+use std::env;
+
+async fn health(headers: HeaderMap) -> String {
+    format!(
+        "Up, answered from 0.0.0.0:{}",
+        env::var("PORT").unwrap_or_else(|_| "".to_string())
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    let port = env::var("PORT").unwrap_or_else(|_| "".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+
+    let app = Router::new().route("/health", get(health));
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+
+    println!("Server running at {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
